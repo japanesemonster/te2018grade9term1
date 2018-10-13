e21ccf1952cbbf92fab9119d065ec1678a461df2
mickell hanchard <mickell051304@gmail.com>
2018-10-13 15:43:41 -0400

    japaneses monster was coding
    hjkjjjkj kj kk k n

--- data.js
+++ data.js
@@ -1,5 +1,7 @@
 // Example
-let message = "sekai wa konnichiwa!";
+let message = " sekai wa konnichiwa";
 console.log(message);
 
 // Type your code below!
+let string = true;
+console.log(typeof(string));

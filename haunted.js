d8ca001b32764a071210cd4d2a9533ea03cb40c8
Reiaz Ramdial <reiazramdial98@gmail.com>
2018-11-03 15:51:36 -0400

    whtas goosd

--- haunted.js
+++ haunted.js
@@ -12,15 +12,16 @@ let player = {
 };
 
 console.log();
-console.log("*** WELCOME TO THE HAUNTED HOUSE ***");
+console.log("*** WELCOME TO THE HOUSE ***");
 player.name = READLINE.question("What is your name?: ");
-console.log(`${player.name} stands in front of a spooky haunted house.`);
-console.log(`${player.name} hears what sounds like screaming coming from inside the house.`);
+console.log(`${player.name} stands in front of a house.`);
+console.log(`${player.name} hears sounds coming from inside the house.`);
 let enterHouse = READLINE.question("Will you enter the house? (yes or no) ");
 if(enterHouse == "y" || enterHouse == "yes") {
   player.courage++;
   console.log(`${player.name} prepares their ${player.weapon} and opens the door...`);
   // continue the story
+"you walk in the house, and...it looks so nice inside! There is a chandelier, and a bear rug, and two sets of stairs...but, it feels weird. You walk further into the house and up the starirs. you see an illuminated room and a shodaw, making groaning noises"
 
 } else {
   player.intellect++;

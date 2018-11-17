9b4f9bd4e9d3bebc6b69c8e27729240641a3669d
Reiaz Ramdial <reiazramdial98@gmail.com>
2018-11-17 12:11:35 -0500

    Update haunted2.js

--- haunted2.js
+++ haunted2.js
@@ -12,11 +12,9 @@ let player = {
 };
 
 console.log();
-console.log("*** WELCOME TO THE HAUNTED HOUSE ***");
-player.name = READLINE.question("What is your name?: ");
-console.log(`${player.name} stands in front of a spooky haunted house.`);
-console.log(`${player.name} hears what sounds like screaming coming from inside the house.`);
-let enterHouse = READLINE.question("Will you enter the house? (yes or no) ");
+  console.log("*** WELCOME TO THE HAUNTED HOUSE ***");
+  player.name = READLINE.question("What is your name?: ");
+    let enterHouse = READLINE.question("Will you enter the house? (yes or no) ");
 if(enterHouse == "y" || enterHouse == "yes") {
   player.courage++;
   console.log(`${player.name} prepares their ${player.weapon} and opens the door...`);
@@ -29,19 +27,37 @@ if(enterDoor == "2"){
 console.log(`You will now be tourchered for the rest of your life`);
 }
 if(enterDoor == "3"){
-console.log(`You will now be to go home SPEAK NOTHING OF THIS`);
+console.log(`You will now go home SPEAK NOTHING OF THIS`);
 }
-  
+
+
+
 } else {
   player.intellect++;
   console.log(`${player.name} decides not to open the door. However...`);
-  console.log(`${player.name} is confronted with another door... Will you open the door`);
+  console.log(` a large furry,large creature in the shadows appears
+    man you dont got no choice now... Will you open the door?`);
   let door =READLINE.question("Do you want to open the door? yes or no");
   if(door == "yes"){
 
-    console.log("You found a frying pan... Good luck you'll need it");
+    console.log("You found a health solution...Good luck you're gonna need it!");
   }
 
-console.log("Thanks for playing!");
+console.log("now there are three more doors");
+let enterDoor = READLINE.question("which do you choose, door 1,2,3?");
+if(enterDoor == "1" ){
+console.log(`you and your relatives now have infinite life but
+now you have to clean for eternity.` );
+}
+if(enterDoor == "2"){
+console.log(`You will be torturered for the rest of your life
+by a herd of zombies and mutant animals. muhahahahaha. `);
+}
+if(enterDoor == "3"){
+console.log(`You are now in a room filled with a thirsty and hungry-cannibal CULT! ninja you dead!`);
+}
+
+
+console.log("Thanks for playin!")
 }
 
